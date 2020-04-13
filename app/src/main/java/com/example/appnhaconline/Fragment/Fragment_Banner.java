991f6642f5053fb6bde7d7ad10442bc0c45a9302
Bui Cao Son <buicaoson.ictu@gmail.com>
2020-04-13 18:48:47 +0700

    Thiet ke layout fragment Quang cao

---
+++ app/src/main/java/com/example/appnhaconline/Fragment/Fragment_Banner.java
@@ -0,0 +1,22 @@
+package com.example.appnhaconline.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.appnhaconline.R;
+
+public class Fragment_Banner extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_banner,container,false);
+        return view;
+    }
+}
