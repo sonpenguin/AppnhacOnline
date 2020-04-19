27eb9490958f0e762341414645d1cf80f071b9ee
Bui Cao Son <buicaoson.ictu@gmail.com>
2020-04-19 21:17:05 +0700

    Thiết kế giao diện màn hình phát nhạc

--- app/src/main/java/com/example/appnhaconline/Activity/PlaynhacActivity.java
+++ app/src/main/java/com/example/appnhaconline/Activity/PlaynhacActivity.java
@@ -1,10 +1,17 @@
 package com.example.appnhaconline.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.appnhaconline.Model.Baihat;
@@ -13,11 +20,17 @@ import com.example.appnhaconline.R;
 import java.util.ArrayList;
 
 public class PlaynhacActivity extends AppCompatActivity {
+    Toolbar toolbarplaynhac;
+    TextView txttimesong, txttotaltimesong;
+    SeekBar sktime;
+    ImageButton imgplay, imgrepeat, imgnext,imgpre,imgrandom;
+    ViewPager viewPagerplaynhac;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playnhac);
+        init();
         Intent intent = getIntent();
         if(intent.hasExtra("cakhuc")){
             Baihat baihat = intent.getParcelableExtra("cakhuc");
@@ -29,5 +42,28 @@ public class PlaynhacActivity extends AppCompatActivity {
                 Log.d("Test",mangbaihat.get(i).getTenBaihat());
             }
         }
+    }
+
+    private void init() {
+        toolbarplaynhac = findViewById(R.id.toolbarplaynhac);
+        txttimesong = findViewById(R.id.textviewtimesong);
+        txttotaltimesong = findViewById(R.id.textviewtotaltimesong);
+        sktime = findViewById(R.id.seekbarsong);
+        imgnext = findViewById(R.id.imagebuttonnext);
+        imgplay = findViewById(R.id.imagebuttonplay);
+        imgpre = findViewById(R.id.imagebuttonpre);
+        imgrandom = findViewById(R.id.imagebuttonshuffle);
+        imgrepeat = findViewById(R.id.imagebuttonrepeat);
+        viewPagerplaynhac = findViewById(R.id.viewpagerplaynhac);
+        setSupportActionBar(toolbarplaynhac);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbarplaynhac.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        toolbarplaynhac.setTitleTextColor(Color.WHITE);
     }
 }
